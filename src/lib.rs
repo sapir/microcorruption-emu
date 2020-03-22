45e08ce199d496cb0deb9dc5d1dababfcf81c63b
sapir <yasapir@gmail.com>
2020-03-22 21:23:38 +0200

    Fix test compilation

--- src/lib.rs
+++ src/lib.rs
@@ -1,69 +1,73 @@
 mod disasm;
 mod emu;
 
-use pyo3::exceptions::ValueError;
-use pyo3::prelude::*;
-use pyo3::wrap_pyfunction;
+#[cfg(not(test))]
+mod pymod {
+    use super::*;
+    use pyo3::exceptions::ValueError;
+    use pyo3::prelude::*;
+    use pyo3::wrap_pyfunction;
 
-#[pyclass]
-struct Emulator {
-    emu: emu::Emulator,
-}
-
-#[pymethods]
-impl Emulator {
-    fn pc(&self) -> u16 {
-        self.emu.pc()
+    #[pyclass]
+    struct Emulator {
+        emu: emu::Emulator,
     }
 
-    fn get_reg(&self, index: u16) -> u16 {
-        self.emu.regs[index]
+    #[pymethods]
+    impl Emulator {
+        fn pc(&self) -> u16 {
+            self.emu.pc()
+        }
+
+        fn get_reg(&self, index: u16) -> u16 {
+            self.emu.regs[index]
+        }
+
+        fn set_reg(&mut self, index: u16, value: u16) {
+            self.emu.regs[index] = value;
+        }
+
+        fn get_byte(&self, addr: u16) -> u8 {
+            self.emu.mem.get_byte(addr)
+        }
+
+        fn get_word(&self, addr: u16) -> u16 {
+            // TODO: don't panic
+            self.emu.mem.get_word(addr).unwrap()
+        }
+
+        fn set_byte(&mut self, addr: u16, value: u8) {
+            self.emu.mem.set_byte(addr, value);
+        }
+
+        fn set_word(&mut self, addr: u16, value: u16) {
+            // TODO: don't panic
+            self.emu.mem.set_word(addr, value).unwrap();
+        }
+
+        fn next_insn(&self) -> PyResult<String> {
+            self.emu
+                .next_insn()
+                .map(|insn| insn.to_string())
+                .map_err(|e| ValueError::py_err(format!("{:?}", e)))
+        }
+
+        fn step(&mut self) {
+            self.emu.step();
+        }
     }
 
-    fn set_reg(&mut self, index: u16, value: u16) {
-        self.emu.regs[index] = value;
+    #[pyfunction]
+    fn load(dump_path: &str) -> PyResult<Emulator> {
+        Ok(Emulator {
+            emu: emu::Emulator::from_dump(dump_path)?,
+        })
     }
 
-    fn get_byte(&self, addr: u16) -> u8 {
-        self.emu.mem.get_byte(addr)
+    #[pymodule]
+    fn msp430emu(_py: Python, m: &PyModule) -> PyResult<()> {
+        m.add_class::<Emulator>()?;
+        m.add_wrapped(wrap_pyfunction!(load))?;
+        Ok(())
     }
-
-    fn get_word(&self, addr: u16) -> u16 {
-        // TODO: don't panic
-        self.emu.mem.get_word(addr).unwrap()
-    }
-
-    fn set_byte(&mut self, addr: u16, value: u8) {
-        self.emu.mem.set_byte(addr, value);
-    }
-
-    fn set_word(&mut self, addr: u16, value: u16) {
-        // TODO: don't panic
-        self.emu.mem.set_word(addr, value).unwrap();
-    }
-
-    fn next_insn(&self) -> PyResult<String> {
-        self.emu
-            .next_insn()
-            .map(|insn| insn.to_string())
-            .map_err(|e| ValueError::py_err(format!("{:?}", e)))
-    }
-
-    fn step(&mut self) {
-        self.emu.step();
-    }
-}
-
-#[pyfunction]
-fn load(dump_path: &str) -> PyResult<Emulator> {
-    Ok(Emulator {
-        emu: emu::Emulator::from_dump(dump_path)?,
-    })
-}
-
-#[pymodule]
-fn msp430emu(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<Emulator>()?;
-    m.add_wrapped(wrap_pyfunction!(load))?;
-    Ok(())
 }
