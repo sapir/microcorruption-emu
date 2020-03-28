e775ad3ee3639f49254e6682ccd407f8dc25d4c3
sapir <yasapir@gmail.com>
2020-03-28 20:45:48 +0300

    Move contents of microcorruption-emu to microcorruption-emu-core

---
+++ microcorruption-emu-core/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod disasm;
+pub mod emu;
+
+pub use disasm::{REG_CG, REG_PC, REG_SP, REG_SR};
+pub use emu::{Emulator, Error, Result};
--- microcorruption-emu/src/lib.rs
+++ microcorruption-emu/src/lib.rs
@@ -1,5 +1,1 @@
-pub mod disasm;
-pub mod emu;
-
-pub use disasm::{REG_CG, REG_PC, REG_SP, REG_SR};
-pub use emu::{Emulator, Error, Result};
+pub use microcorruption_emu_core::*;
